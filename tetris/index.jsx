415f1a07c1b18cf1e41a0ac6c3080c199f038436
James Harris <wizardofmath@gmail.com>
2014-11-13 18:59:09 -0700

    Another prototype tetris

--- tetris/index.jsx
+++ tetris/index.jsx
@@ -3,37 +3,59 @@ var GameBox = React.createClass({
         return {gameState:this.props.gameState,ticked:0};
     },
      componentDidMount: function(){
-
         // componentDidMount is called by react when the component
         // has been rendered on the page. We can set the interval here:
-        this.gravity = setInterval(this.tick, 10);
+        this.tick = setInterval(this.gravity, 1000);
     },
 
     componentWillUnmount: function(){
-
         // This method is called immediately before the component is removed
         // from the page and destroyed. We can clear the interval here:
-        clearInterVal(this.gravity);
+        clearInterVal(this.tick);
     },
 
-    tick: function(){
-
-        // This function is called every 50 ms. It updates the
-        // elapsed counter. Calling setState causes the component to be re-rendered
-        console.log("Ticked" + this.state.ticked);
-        this.state.gameState.currentPiece= randPiece().movePieceDown();
-        this.setState({gameState:this.state.gameState,ticked:this.state.ticked+1});
+    addPiece: function(){
+        this.state.gameState.currentPiece = new Piece(randPiece());
+        this.setState({gameState:this.state.gameState});
+    },dropPiece: function(){
+        this.state.gameState.currentPiece.dropPiece(this.state.gameState.board());
+        this.state.gameState.addPiece(this.state.gameState.currentPiece);
+        this.state.gameState.currentPiece = new Piece(randPiece());
+        this.setState({gameState:this.state.gameState});
+    },rotatePiece: function(){
+        this.state.gameState.currentPiece.rotateClockWise(this.state.gameState.board());
+        this.setState({gameState:this.state.gameState});
+    },moveLeft: function(){
+        this.state.gameState.currentPiece.shiftLeft(this.state.gameState.board());
+        this.setState({gameState:this.state.gameState});
+    },moveRight: function(){
+        this.state.gameState.currentPiece.shiftRight(this.state.gameState.board());
+        this.setState({gameState:this.state.gameState});
+    },gravity: function(){
+        if(this.state.gameState.currentPiece && !this.state.gameState.gameOver){
+            console.log(this.state.gameState.currentPiece.movePieceDown(this.state.gameState.board()));
+            this.setState({gameState:this.state.gameState});
+        }
     },
     render: function() {
+        if(this.state.gameState.gameOver){
+            alert("Gameover");
+        };
         return (
-            <div className="GameBox">{this.state.gameState.gameOver +" " + this.state.theTick} <button onClick={this.tick}>Add Piece</button>
+            <div className="GameBox">
+
+            <button onClick={this.addPiece}>Add Piece</button>
+            <button onClick={this.dropPiece}>Drop Piece</button>
+            <button onClick={this.gravity}>Soft Drop Piece</button>
+            <button onClick={this.rotatePiece}>Rotate</button>
+            <button onClick={this.moveLeft}>Move Left</button>
+            <button onClick={this.moveRight}>Move Right</button>
+
             {this.state.gameState.board().map(function(row) {
                 return <div className="row">{row.map(function(cell) {
-                    var cellState= cell.occupied ? "occupied" : "free";
                     return <div className={"cell"} style={{"backgroundColor": cell.color}}></div>;
             })}</div>;
             })}
-            {console.log(this.state.gameState.cells)}
             </div>
         )
     }
--- tetris/js/gameEngine.js
+++ tetris/js/gameEngine.js
@@ -2,64 +2,70 @@ var gameEngine = {
     initialgameState: function () {
         var x = {};
         x.gameOver = false;
+        x.currentPiece = new Piece(randPiece());
         x.heldPiece = {};
-        x.currentPiece = new Piece();
-        x.cells = [];
-        for (var i = 0; i < 22; i++) {
-            x.cells.push([]);
-            for (var j = 0; j < 10; j++) {
-                x.cells[i].push({
-                    occupied: false,
-                    color: "grey"
-                });
-            }
-        }
-        x.peices = new Array();
+        x.peices = [];
         x.board = function () {
-            var currentBoard = x.cells.map(function (row) {
-                return row.map(function (cell) {
-                    return cell;
-                })
-            });
-            if (x.peices.length > 0) {
-                x.peices.map(function (pieceToDisplay) {
-                    pieceToDisplay.cells.map(function (cell) {
-                       currentBoard[cell.y][cell.x] = {occupied: true,color: pieceToDisplay.color};
-                    })
-                });
+            var currentBoard = [[]];
+            console.log(x.peices);
+            for (var i = 0; i < 22; i++) {
+                currentBoard[i] = [];
+                for (var j = 0; j < 10; j++) {
+                    currentBoard[i][j] = {
+                        x: i,
+                        y: j,
+                        color: "lightgrey",
+                        occupied: false
+                    };
+                }
             }
-            if (x.currentPiece && x.currentPiece.color!="brown") {
-                x.currentPiece.cells.map(function (cell) {
-                    currentBoard[cell.y][cell.x] = {occupied: true,color: x.currentPiece.color};
+            x.peices.map(function (cell) {
+                if (0 <= cell.y <= 21 && 0 <= cell.x <= 9) {
+                    if (currentBoard[cell.y] && currentBoard[cell.y][cell.x]) {
+                        currentBoard[cell.y][cell.x] = {
+                            x: cell.x,
+                            y: cell.y,
+                            color: cell.color,
+                            occupied: true
+                        };
+                    }
+                }
+            });
+            if (x.currentPiece && x.currentPiece.color != "brown") {
+                x.currentPiece.cells().map(function (cell) {
+                    if (0 <= cell.y <= 21 && 0 <= cell.x <= 9) {
+                        currentBoard[cell.y][cell.x] = {
+                            x: cell.x,
+                            y: cell.y,
+                            color: x.currentPiece.color(),
+                            occupied: false
+                        };
+                    }
                 });
             }
             return currentBoard;
-        }
+        };
         x.addPiece = function (piece) {
             var collides = false;
-            this.peices.map(function (pieceToComp) {
-                if (pieceToComp.collides(piece)) {
-                    collides = true;
-                }
-            })
-            if (!collides) {
-                this.peices.push(piece);
+            console.log("In Add Piece:");
+            console.log(piece);
+            var collided = x.peices.filter(function (cell) {
+                return piece.collidesWithCell(cell);
+            });
+            if (piece == x.currentPiece) {
+                x.currentPiece = new Piece(randPiece());
             }
-            if(!collides&&piece==x.currentPiece){
-                x.currentPiece = new Piece();
+            if (!collided.length > 0) {
+                piece.cells().map(function (cell) {
+                    x.peices.push(cell);
+                });
             }
-            if(collides){
+            if (collided.length > 0) {
                 x.gameOver = true;
             }
+            console.log("Collided Cells:" + collided.length);
             return this;
-        }
-        x.dropPiece = function (piece) {
-            var currentBoard = this.board();
-            while (piece.canMoveDown(currentBoard)) {
-                piece = piece.movePieceDown();
-            }
-            return piece;
-        }
+        };
         return x;
     },
     rotatePiece: {},
@@ -67,204 +73,4 @@ var gameEngine = {
 
 };
 
-function Piece(name, cells, color) {
-    this.name = name ? name : "I";
-    this.color = color ? color : "brown";
-    this.cells = cells ? cells : [
-        {
-            x: 2,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 4,
-            y: 0
-        },
-        {
-            x: 5,
-            y: 0
-        }
-    ];
-}
-
-Piece.prototype.collides = function (piece) {
-    var collides = false;
-    this.cells.map(function (cell) {
-        piece.cells.map(function (cellToComp) {
-            if (cell.x == cellToComp.x && cell.y == cellToComp.y) {
-                collides = true;
-            }
-        });
-    });
-    return collides;
-}
-Piece.prototype.movePieceDown = function () {
-    var newPiece = new Piece(this.name, this.cells.map(function (cell) {
-        var newCell = {
-            x: cell.x,
-            y: cell.y + 1
-        };
-        return newCell;
-    }), this.color)
-    return newPiece;
-};
-
-Piece.prototype.canMoveDown = function (currentBoard) {
-    var canMoveDown = true;
-    this.cells.map(function (cell) {
-        if (cell.y + 1 > 21) {
-            canMoveDown = false;
-        }
-        if (canMoveDown && currentBoard[cell.y + 1][cell.x].occupied) {
-            canMoveDown = false;
-        }
-    })
-    return canMoveDown;
-};
-
-var pieces = [];
-(function () {
-    pieces.push(new Piece("I", [
-        {
-            x: 2,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 4,
-            y: 0
-        },
-        {
-            x: 5,
-            y: 0
-        }
-    ], "Cyan"));
-    pieces.push(new Piece("O", [
-        {
-            x: 2,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 2,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 1
-        }
-    ], "Yellow"));
-    pieces.push(new Piece("T", [
-        {
-            x: 2,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 1
-        },
-        {
-            x: 4,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 0
-        }
-    ], "Purple"));
-    pieces.push(new Piece("S", [
-        {
-            x: 2,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 4,
-            y: 0
-        }
-    ], "Green"));
-    pieces.push(new Piece("Z", [
-        {
-            x: 2,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 1
-        },
-        {
-            x: 4,
-            y: 1
-        }
-    ], "Red"));
-    pieces.push(new Piece("J", [
-        {
-            x: 2,
-            y: 1
-        },
-        {
-            x: 2,
-            y: 0
-        },
-        {
-            x: 3,
-            y: 0
-        },
-        {
-            x: 4,
-            y: 0
-        }
-    ], "Blue"));
-    pieces.push(new Piece("L", [
-        {
-            x: 2,
-            y: 1
-        },
-        {
-            x: 3,
-            y: 1
-        },
-        {
-            x: 4,
-            y: 1
-        },
-        {
-            x: 4,
-            y: 0
-        }
-    ], "Orange"));
-})();
-
-var randPiece = function () {
-    return pieces[Math.floor((Math.random() * 7))];
-};
-
 var gameA = gameEngine.initialgameState();
-gameA.addPiece(gameA.dropPiece(randPiece()));
-gameA.addPiece(gameA.dropPiece(randPiece()));
-gameA.addPiece(gameA.dropPiece(randPiece()));
-gameA.addPiece(gameA.dropPiece(randPiece()));
-gameA.addPiece(gameA.dropPiece(randPiece()));
-gameA.addPiece(gameA.dropPiece(randPiece()));
-
-
